e303198eb4fc26d5e436bfce2971ac7b6cd46cd8
AbduazizBobomalikovNode <86071431+AbduazizBobomalikovNode@users.noreply.github.com>
2023-12-09 13:05:46 +0500

    update

--- views/public/asserts/js/table_view.js
+++ views/public/asserts/js/table_view.js
@@ -1,4 +1,5 @@
-let tr_count = 1;
+let tr1_count = 1;
+let tr2_count = 1;
 let array = document.getElementsByTagName("tr");
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
@@ -10,17 +11,35 @@ for (let index = 0; index < array.length; index++) {
 
 let button_add_tr = document.getElementById("table-dynamik+");
 let button_delete_tr = document.getElementById("table-dynamik-");
-let list_tr = document.getElementById('table2').children;
+let button1_add_tr = document.getElementById("table-dynamik1+");
+let button1_delete_tr = document.getElementById("table-dynamik1-");
+
+let list1_tr = document.getElementById('table2').children;
+let list2_tr = document.getElementById('table3').children;
+
 
 button_delete_tr.onclick = () => {
-    if (tr_count > 1) {
-        tr_count--;
-        list_tr[tr_count].style.display = "none";
+    if (tr1_count > 1) {
+        tr1_count--;
+        list1_tr[tr1_count].style.display = "none";
     }
 }
 button_add_tr.onclick = () => {
-    if (tr_count < 8) {
-        list_tr[tr_count].style.display = "table-row";
-        tr_count++;
+    if (tr1_count < 8) {
+        list1_tr[tr1_count].style.display = "table-row";
+        tr1_count++;
+    }
+};
+
+button1_add_tr.onclick = () => {
+    if (tr2_count < 8) {
+        list2_tr[tr2_count].style.display = "table-row";
+        tr2_count++;
+    }
+};
+button1_delete_tr.onclick = () => {
+    if (tr2_count > 1) {
+        tr2_count--;
+        list2_tr[tr2_count].style.display = "none";
     }
 }
